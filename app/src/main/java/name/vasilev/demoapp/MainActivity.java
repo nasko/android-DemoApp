2afef810d6afcb0d75945e02f47e9bb39f9d27f0
Atanas Vasilev <atanas.vasilev@gmail.com>
2017-07-11 22:23:27 +0300

    Small assignment

--- app/src/main/java/name/vasilev/demoapp/MainActivity.java
+++ app/src/main/java/name/vasilev/demoapp/MainActivity.java
@@ -2,8 +2,15 @@ package name.vasilev.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        Log.i("Info", "Button pressed!");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
